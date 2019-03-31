1e52be8dc666c5b9ec3ebc992ce93cdad4247b0b
mambax7 <mambax7@gmail.com>
2019-03-31 09:40:59 -0400

    English

--- language/english/main.php
+++ language/english/main.php
@@ -24,61 +24,67 @@
 include_once 'common.php';
  
 // ---------------- Main ----------------
-define('_MA_WGREALESTATE_INDEX', 'Übersicht');
-define('_MA_WGREALESTATE_BACK_INDEX', 'Zurück zur Übersicht');
-define('_MA_WGREALESTATE_BACK_OBJECT', 'Zurück zum Objekt');
-define('_MA_WGREALESTATE_BACK_OBJECT_EDIT', 'Zurück zur Objektbearbeitung');
+define('_MA_WGREALESTATE__INDEX', 'Overview');
+define('_MA_WGREALESTATE_BACK_INDEX', 'Back to overview');
+define('_MA_WGREALESTATE_BACK_OBJECT', 'Back to Object');
+define('_MA_WGREALESTATE_BACK_OBJECT_EDIT', 'Back to object editing');
 
 define('_MA_WGREALESTATE_TITLE', 'wgRealEstate');
-define('_MA_WGREALESTATE_DESC', 'This module provides a platform for selling/renting real estates');
+define('_MA_WGREALESTATE_DESC', 'This module provides a platform for selling / renting real estates');
 define('_MA_WGREALESTATE_NO', 'No');
 // ---------------- Contents ----------------
 
 // Object
-define('_MA_WGREALESTATE_OBJECT', 'Objekt');
-define('_MA_WGREALESTATE_OBJECTS_TITLE', 'Liste der derzeit verfügbaren Objekte');
-define('_MA_WGREALESTATE_OBJECT_GEOCOORDS', 'Hier befindet sich Ihre zukünftige Immobilie');
-define('_MA_WGREALESTATE_OBJECTS_NOIMAGE', 'Für diese Immobilie ist derzeit kein Bild verfügbar');
-define('_MA_WGREALESTATE_OBJECTS_STATE', 'Aktueller Status');
+define('_MA_WGREALESTATE_OBJECT', 'Object');
+define('_MA_WGREALESTATE_OBJECTS_TITLE', 'List of currently available objects');
+define('_MA_WGREALESTATE_OBJECT_GEOCOORDS', 'Here is your future property');
+define('_MA_WGREALESTATE_OBJECTS_NOIMAGE', 'There is currently no image available for this property');
+define('_MA_WGREALESTATE_OBJECTS_STATE', 'Current Status');
 // Attributes
-define('_MA_WGREALESTATE_ATTRIBUTE', 'Objektattribut');
-define('_MA_WGREALESTATE_ATTRIBUTES', 'Objektattribute');
-define('_MA_WGREALESTATE_ATTRIBUTES_TITLE', 'Definieren der Attribute eines Objektes');
-// Kostenart
-define('_MA_WGREALESTATE_COST_TYPE', 'Kostenart');
-define('_MA_WGREALESTATE_COST_TYPES', 'Kostenarten');
-// Kosten
-define('_MA_WGREALESTATE_COST', 'Kosten');
-define('_MA_WGREALESTATE_COSTS', 'Kosten');
-define('_MA_WGREALESTATE_COSTS_TITLE', 'Bearbeiten der Kosten eines Objektes');
-define('_MA_WGREALESTATE_COST_SUM', 'Summe');
+define('_MA_WGREALESTATE_ATTRIBUTE', 'Object attribute');
+define('_MA_WGREALESTATE_ATTRIBUTES', 'Object Attributes');
+define('_MA_WGREALESTATE_ATTRIBUTES_TITLE', 'Defining the attributes of an object');
+// cost type
+define('_MA_WGREALESTATE_COST_TYPE', 'Cost type');
+define('_MA_WGREALESTATE_COST_TYPES', 'Cost Types');
+// costs
+define('_MA_WGREALESTATE_COST', 'Costs');
+define('_MA_WGREALESTATE_COSTS', 'Costs');
+define('_MA_WGREALESTATE_COSTS_TITLE', 'Edit the cost of an object');
+define('_MA_WGREALESTATE_COST_SUM', 'Sum');
 // Contact
-define('_MA_WGREALESTATE_CONTACT', 'Verkäufer kontaktieren');
-define('_MA_WGREALESTATE_CONTACT_NAME', 'Ihr Name');
-define('_MA_WGREALESTATE_CONTACT_NAME_INFO', 'Bitte Ihren Namen angeben');
-define('_MA_WGREALESTATE_CONTACT_PHONE', 'Ihre Telefonnummer');
-define('_MA_WGREALESTATE_CONTACT_PHONE_INFO', 'Bitte Ihre Telefonnummer angeben');
-define('_MA_WGREALESTATE_CONTACT_MAIL', 'Ihre E-Mail-Adresse');
-define('_MA_WGREALESTATE_CONTACT_MAIL_INFO', 'Bitte Ihre E-Mail-Adresse angeben');
-define('_MA_WGREALESTATE_CONTACT_SUBJECT', 'Betreff');
-define('_MA_WGREALESTATE_CONTACT_MESSAGE', 'Nachricht');
-define('_MA_WGREALESTATE_CONTACT_MESSAGE_INFO', 'Hier können Sie noch zusätzliche Infos/Wünsche angeben');
-define('_MA_WGREALESTATE_CONTACT_SUBMIT', 'Anfrage absenden');
-define('_MA_WGREALESTATE_CONTACT_CONFIRM', 'Eine Kopie der Anfrage an die von mir angegebene E-Mail-Adresse senden?');
-define('_MA_WGREALESTATE_CONTACT_SUBMIT_SUCCESS', 'Ihre Anfrage wurde erfolgreich gesendet. Wir werden uns umgehend bei Ihnen melden.');
-define('_MA_WGREALESTATE_CONTACT_SUBMIT_FAILED', 'Beim Senden der Anfrage ist leider ein Fehler aufgetreten. Bitte kontaktieren Sie uns direkt.');
-// Admin link
+define('_MA_WGREALESTATE_CONTACT', 'Contact seller');
+define('_MA_WGREALESTATE_CONTACT_NAME', 'Your name');
+define('_MA_WGREALESTATE_CONTACT_NAME_INFO', 'Please specify your name');
+define('_MA_WGREALESTATE_CONTACT_PHONE', 'Your phone number');
+define('_MA_WGREALESTATE_CONTACT_PHONE_INFO', 'Please enter your phone number');
+define('_MA_WGREALESTATE_CONTACT_MAIL', 'Your e-mail address');
+define('_MA_WGREALESTATE_CONTACT_MAIL_INFO', 'Please enter your e-mail address');
+define('_MA_WGREALESTATE_CONTACT_SUBJECT', 'Subject');
+define('_MA_WGREALESTATE_CONTACT_MESSAGE', 'Message');
+define('_MA_WGREALESTATE_CONTACT_MESSAGE_INFO', 'Here you can specify additional information / requests');
+define('_MA_WGREALESTATE_CONTACT_SUBMIT', 'Submit Request');
+define('_MA_WGREALESTATE_CONTACT_CONFIRM', 'Send a copy of the request to the e-mail address I have provided?');
+define('_MA_WGREALESTATE_CONTACT_SUBMIT_SUCCESS', 'Your request has been sent successfully and we will get back to you as soon as possible.');
+define('_MA_WGREALESTATE_CONTACT_SUBMIT_FAILED', 'Sorry, there was an error sending the request, please contact us directly.');
+// admin link
 define('_MA_WGREALESTATE_ADMIN', 'Admin');
 //General
-define('_MA_WGREALESTATE_SHOW', 'Details anzeigen');
-define('_MA_WGREALESTATE_SORTABLE_MOVE', 'Reihenfolge ändern');
-define('_MA_WGREALESTATE_EDIT_START', 'Details bearbeiten');
-define('_MA_WGREALESTATE_EDIT_END', 'Bearbeitung Details beenden');
+define('_MA_WGREALESTATE_SHOW', 'Show details');
+define('_MA_WGREALESTATE_SORTABLE_MOVE', 'Change order');
+define('_MA_WGREALESTATE_EDIT_START', 'Edit Details');
+define('_MA_WGREALESTATE_EDIT_END', 'Finish editing details');
 define('_MA_WGREALESTATE_DELETE', 'Delete object');
-define('_MA_WGREALESTATE_TYPE_VALID', 'Gültig');
-define('_MA_WGREALESTATE_DATECREATE', 'Erstellt am');
-define('_MA_WGREALESTATE_SUBMITTER', 'Ersteller');
+define('_MA_WGREALESTATE_TYPE_VALID', 'Valid');
+define('_MA_WGREALESTATE_DATECREATE', 'Submitted on');
+define('_MA_WGREALESTATE_SUBMITTER', 'Submitter');
 define('_MA_WGREALESTATE_TYPE_INFO', 'Info');
 // google map
-define('_MA_WGREALESTATE_GM_SEARCHBOX', 'Bitte Suchfilter eingeben');
+define('_MA_WGREALESTATE_GM_SEARCHBOX', 'Please enter search filter');
+
+define('_MA_WGREALESTATE_INDEX', 'Home');
+
+
+
+
 // ---------------- End ----------------
